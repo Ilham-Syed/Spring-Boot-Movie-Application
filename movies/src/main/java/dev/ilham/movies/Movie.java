148fd75b230b14a4bd94c19ed74226b277f38788
Ilham-Syed <ilhamsyed1310@gmail.com>
2024-05-24 01:36:14 +0530

    Working on the backend API

---
+++ movies/src/main/java/dev/ilham/movies/Movie.java
@@ -0,0 +1,28 @@
+package dev.ilham.movies;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genre;
+    private List<String> backdrops;
+    private List<Review> reviewIds;
+}
---
+++ movies/src/main/java/dev/ilham/movies/Review.java
@@ -0,0 +1,16 @@
+package dev.ilham.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+}
