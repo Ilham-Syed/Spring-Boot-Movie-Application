453369f741cb1b8fd363b30cb75f368aeaac4a04
Ilham-Syed <ilhamsyed1310@gmail.com>
2024-05-25 01:43:08 +0530

    Movie controller completed

--- movies/src/main/java/dev/ilham/movies/MovieRepository.java
+++ movies/src/main/java/dev/ilham/movies/MovieRepository.java
@@ -4,7 +4,10 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-
+    Optional<Movie> findMoviesByImdbId(String imdbId);
 }
--- movies/src/main/java/dev/ilham/movies/MovieService.java
+++ movies/src/main/java/dev/ilham/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.ilham.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,5 +14,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> AllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getSingleMovie(String imdbId){
+        return movieRepository.findMoviesByImdbId(imdbId);
     }
 }
